3e6733629eb67360dc08f9429ec46e2e6de86540
github-action-benchmark <github@users.noreply.github.com>
2021-12-29 08:31:02 +0000

    add Benchmark (benchmarkjs) benchmark result for 3f8ee8b9272cc0523a3e9a55de512ccdf520a0f8

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1640766662038,
+  "repoUrl": "https://github.com/speedy-js/speedy-native",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "speedy-js",
+            "username": "speedy-js"
+          },
+          "committer": {
+            "name": "speedy-js",
+            "username": "speedy-js"
+          },
+          "id": "3f8ee8b9272cc0523a3e9a55de512ccdf520a0f8",
+          "message": "ci: add benchmark report",
+          "timestamp": "2021-12-29T07:03:59Z",
+          "url": "https://github.com/speedy-js/speedy-native/pull/8/commits/3f8ee8b9272cc0523a3e9a55de512ccdf520a0f8"
+        },
+        "date": 1640766660673,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Babel",
+            "value": 1275,
+            "range": "±12.48%",
+            "unit": "ops/sec",
+            "extra": "79 samples"
+          },
+          {
+            "name": "Rust",
+            "value": 14715,
+            "range": "±0.15%",
+            "unit": "ops/sec",
+            "extra": "96 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
