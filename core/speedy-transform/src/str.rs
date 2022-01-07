c4af4d41edb61d4338422fc68ee22662fcca1e66
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-01-07 15:52:38 +0800

    chore(release): publish fix(speedy-transform): fix reactruntime jundge

---
+++ core/speedy-transform/src/str.rs
@@ -0,0 +1,11 @@
+pub trait StringExtend {
+  fn compare_handle(&self) -> std::string::String;
+}
+
+impl StringExtend for String {
+  fn compare_handle(&self) -> std::string::String {
+    let mut new_str = self.replace(" ", "");
+    new_str = new_str.trim().replace("\n", "");
+    new_str
+  }
+}
